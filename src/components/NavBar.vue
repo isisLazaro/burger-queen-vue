91c38490f26568f1d6ff6363a3251c766f425d0f
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-09-17 17:23:37 -0500

    Creacion de componentes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <p>holi soy la navbar</p>
+        <!--
+            1. menu hamburguesa
+            2.logo
+            3. comandas
+        -->
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: "NavBar",
+  /* components: {
+  } */
+};
+</script>

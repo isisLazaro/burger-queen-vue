0c56cf76d9c2098604b3ec9fb30069cd1197f2ff
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-09-18 18:35:04 -0500

    v1

--- src/components/TipoServicio.vue
+++ src/components/TipoServicio.vue
@@ -8,6 +8,7 @@
         </button>
     </div>
 </template>
+
 <script>
 export default {
   name: 'TipoServicio'
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,7 +19,6 @@ export default {
   components: {
     NavBar,
     EligeSucursal
-
   }
 };
 </script>

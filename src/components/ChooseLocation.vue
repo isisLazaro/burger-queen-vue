76b6312159053c871db6e93ba3d044134a34275a
Isis Lázaro <isislazaro@gmail.com>
2019-09-18 18:28:39 -0500

    Cambio de nombre de componentes a inglés. Creación de componentes de la vista order

---
+++ src/components/ChooseLocation.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <p>Holi mesera!</p>
+      <p>Elige tu surcusal</p>
+      <button>Chilpancingo</button>
+      <button>Orizaba</button>
+    </div>
+    <div>
+      <p>Apretura de caja</p>
+      <input type="number" />
+    </div>
+    <button type="submit">Aceptar</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "ChooseLocation"
+  /* components: {
+  } */
+};
+</script>

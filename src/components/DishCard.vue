ac3ade89c9ff465f5a8429cab4d8805b3d03516a
Isis Lázaro <isislazaro@gmail.com>
2019-09-20 17:34:22 -0500

    se agregó la data, tanto en la vista como en el elemento, en forma de un array de objeto, y despliega una sola tarjeta

--- src/components/DishCard.vue
+++ src/components/DishCard.vue
@@ -1,15 +1,32 @@
 <template>
   <div>
- <!--fotos y con diferentes id-->   
+    <!--fotos y con diferentes id-->
     <figure>
-      <img src alt="foto platillo" />
+      <img :src="dishes[0].foto" alt="foto platillo" width="100" />
     </figure>
-    <p>Nombre Platillo</p>
+    <p>{{ dishes[0].name }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "DishCard"
+  name: "DishCard",
+  data() {
+    return {
+      dishes: [
+        {
+          name: "HAMBURGUESA",
+          tipo: "un nombre que no me acuerdo en este momento",
+          foto:
+            "https://image.freepik.com/free-vector/burger-vector-illustration-simple-element-illustration-king-burger-with-crown-logo-vector_101969-3.jpg"
+        },
+        {
+          name: "JOCHO",
+          tipo: "norteño",
+          foto: "https://image.freepik.com/free-vector/hot-dog_53876-43863.jpg"
+        }
+      ]
+    };
+  }
 };
-</script> 
+</script>
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <Service></Service>
     <ChooseTable></ChooseTable>
-    <DishCard></DishCard>
+    <DishCard />
+
     <ShoppingCart></ShoppingCart>
     <!--
             1.(x)navbar 
@@ -28,6 +29,23 @@ export default {
     ChooseTable,
     DishCard,
     ShoppingCart
+  },
+  data() {
+    return {
+      dishes: [
+        {
+          name: "HAMBURGUESa",
+          tipo: "un nombre que no me acuerdo en este momento",
+          foto:
+            "https://image.freepik.com/free-vector/burger-vector-illustration-simple-element-illustration-king-burger-with-crown-logo-vector_101969-3.jpg"
+        },
+        {
+          name: "JOCHO",
+          tipo: "norteño",
+          foto: "https://image.freepik.com/free-vector/hot-dog_53876-43863.jpg"
+        }
+      ]
+    };
   }
 };
 </script>

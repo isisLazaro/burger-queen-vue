8a456d95f45fc5ff161caa6b97832f427597b035
Isis Lázaro <isislazaro@gmail.com>
2019-09-23 09:22:13 -0500

    Se creó ruta para editar pedido y se ligo con la tarjeta de tipo de platillo. También se añadió funcionalidad para aumentar o disminuir cantidad de platillos, y mostrar la cantidad de platillos a ordenar

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Order from "./views/Order.vue";
 import Dish from "./views/Dish.vue";
+import EditOrder from "./views/EditOrder.vue";
 
 Vue.use(Router);
 
@@ -23,6 +24,11 @@ export default new Router({
       name: "Platillo",
       component: Dish,
       props: true
+    },
+    {
+      path: "/tomacomanda/:dish/editarplatillo",
+      name: "EditarPlatillo",
+      component: EditOrder
     }
   ]
 });

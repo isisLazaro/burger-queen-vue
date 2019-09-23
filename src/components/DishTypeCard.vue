46f67dc5dd5dfc4d527089a5a9618f260dce69df
Isis Lázaro <isislazaro@gmail.com>
2019-09-23 02:33:08 -0500

    se creo estructura tarjeta para selecconar tipo de platillo, tipo de orden, cantidad y editar pedido. se utiliza el mismo formato para todos los platillos

---
+++ src/components/DishTypeCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Nombre Platillo</p>
+    <button>combo</button>
+    <button>sencillo</button>
+    <button>+</button>
+    <input type="text" name id />
+    <button>-</button>
+    <button>editar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DishTypeCard"
+};
+</script>
--- src/views/Dish.vue
+++ src/views/Dish.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>{{ dish }}</h1>
+    <DishTypeCard></DishTypeCard>
     <router-link :to="{ name: 'TomaComanda' }">regresar</router-link>
   </div>
 </template>
 
 <script>
+import DishTypeCard from "@/components/DishTypeCard.vue";
 export default {
-  props: ["dish"]
+  name: "Dish",
+  props: ["dish"],
+  components: {
+    DishTypeCard
+  }
 };
 </script>

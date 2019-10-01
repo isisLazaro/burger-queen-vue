ad952061a42e927863fa59b8d2649bfbef1e6668
Isis Lázaro <isislazaro@gmail.com>
2019-10-01 10:33:36 -0500

    se creó elemento de apertura de caja y se colocó en la vistan home

--- src/components/ChooseLocation.vue
+++ src/components/ChooseLocation.vue
@@ -1,17 +1,9 @@
 <template>
   <div>
-    <div>
-      <p>Elige tu surcusal</p>
-      <!--aqui va el boton que me lleva a view Order.vue-->
-      <button>Orizaba</button>
-      <button>Chilpancingo</button>
-    </div>
-    <div>
-      <p>Apretura de caja</p>
-      <input type="number" />
-    </div>
-    <v-btn>ACEPTAR</v-btn>
-    <router-link :to="{ name: 'TomaComanda' }">ACEPTAR</router-link>
+    <p>Elige tu surcusal</p>
+    <!--aqui va el boton que me lleva a view Order.vue-->
+    <button>Orizaba</button>
+    <button>Chilpancingo</button>
   </div>
 </template>
 <script>

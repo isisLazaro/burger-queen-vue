822bb2a24bf295436632f8869f3740dfc5fab944
Isis Lázaro <isislazaro@gmail.com>
2019-10-05 23:00:35 -0500

    alineación elementos de la tarjeta

--- src/components/DishTypeCard.vue
+++ src/components/DishTypeCard.vue
@@ -1,48 +1,42 @@
 <template>
-
-
   <v-card class="mx-auto" width="600">
-    <v-card-title>{{ name }}</v-card-title>
-
+    <!-- <v-card-title>{{ name }}</v-card-title> -->
+    <v-card-title>Nombre Platillo</v-card-title>
     <v-divider />
     <v-card-text>
-      <v-container>
-        <v-row no-gutters>
-          <v-col cols="5">
-            <v-btn>combo</v-btn>
-            <v-btn>sencillo</v-btn>
-          </v-col>
-          <v-col cols="6">
-            <v-row no-gutters>
-              <v-col>
-                <v-btn v-on:click="addDish" left="true">+</v-btn>
-              </v-col>
-              <v-col>
-                <v-text-field v-model="numDishes" id readonly solo />
-              </v-col>
-              <v-col>
-                <v-btn v-on:click="removeDish" :disabled="numDishes < 1"
-                  >-</v-btn
-                >
-              </v-col>
-            </v-row>
-          </v-col>
-          <v-col cols="1" class="fill-height align-end">
-            <router-link :to="{ name: 'EditarPlatillo' }">
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-            </router-link>
-          </v-col>
-        </v-row>
-      </v-container>
+      <v-row no-gutters align="center">
+        <v-col cols="6">
+          <v-btn x-small>combo</v-btn>
+          <v-btn x-small>sencillo</v-btn>
+        </v-col>
+        <v-col cols="5">
+          <v-row no-gutters align="center">
+            <v-col>
+              <v-btn v-on:click="addDish" x-small>+</v-btn>
+            </v-col>
+            <v-col>
+              <v-text-field v-model="numDishes" id readonly solo width="50vw" />
+            </v-col>
+            <v-col>
+              <v-btn v-on:click="removeDish" x-small :disabled="numDishes < 1"
+                >-</v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="1" class="fill-height align-end">
+          <router-link :to="{ name: 'EditarPlatillo' }">
+            <v-btn icon>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-
-
 export default {
   name: "DishTypeCard",
   data() {
@@ -55,17 +49,16 @@ export default {
       type: String,
       required: true
     }
-    },
-    methods: {
+  },
+  methods: {
     addDish() {
       this.numDishes += 1;
     },
     removeDish() {
       this.numDishes -= 1;
     }
-    }
   }
-
+};
 </script>
 
 <style scoped></style>

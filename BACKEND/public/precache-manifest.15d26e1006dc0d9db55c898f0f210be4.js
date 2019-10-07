369570a069f81d95c9fcf510db1c5118bc100bf4
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-10-07 12:50:10 -0500

    cambios bck

---
+++ BACKEND/public/precache-manifest.15d26e1006dc0d9db55c898f0f210be4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "5fd953d70ca893ed35a3",
+    "url": "js/chunk-vendors.158c8bb6.js"
+  },
+  {
+    "revision": "c89d23d3216bab150b13",
+    "url": "js/app.5a809de4.js"
+  },
+  {
+    "revision": "b9d3b979c3717c2acb9d09b9ebe4f3de",
+    "url": "index.html"
+  },
+  {
+    "revision": "db25515e6f9cb3a1c8d7fb81daeb12a5",
+    "url": "img/logo-comdi.db25515e.png"
+  },
+  {
+    "revision": "5fd953d70ca893ed35a3",
+    "url": "css/chunk-vendors.3f6a6465.css"
+  },
+  {
+    "revision": "c89d23d3216bab150b13",
+    "url": "css/app.2e9eaaf3.css"
+  }
+];

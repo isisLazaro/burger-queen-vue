fa8f6e3f6f8c4339277349f24303d18a9c9aa6e3
Isis Lázaro <isislazaro@gmail.com>
2019-10-07 09:28:08 -0500

    formato font buttons

--- src/components/DishTypeCard.vue
+++ src/components/DishTypeCard.vue
@@ -1,41 +1,37 @@
 <template>
-
-
   <v-card class="mx-auto" width="600">
     <v-card-title>{{ name }}</v-card-title>
     <v-divider />
     <v-card-text>
-      <v-container>
-        <v-row no-gutters>
-          <v-col cols="5">
-            <v-btn>Combo {{ combo }}</v-btn>
-            <v-btn>Sencilla {{ sencilla}}</v-btn>
-          </v-col>
-          <v-col cols="6">
-           <!--  <v-row no-gutters>
-              <!--aqui deben mandar la info a el carrito de compras 
-              <v-col>
-                <v-btn v-on:click="addDish" left="true">+</v-btn>
-              </v-col>
-              <v-col>
-                <v-text-field v-model="numDishes" id readonly solo />
-              </v-col>
-              <v-col>
-                <v-btn v-on:click="removeDish" :disabled="numDishes < 1"
-                  >-</v-btn
-                >
-              </v-col>
-            </v-row> -->
-          </v-col>
-          <v-col cols="1" class="fill-height align-end">
-            <router-link :to="{ name: 'EditarPlatillo' }">
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-            </router-link>
-          </v-col>
-        </v-row>
-      </v-container>
+      <v-row no-gutters align="center">
+        <v-col cols="6">
+          <v-btn small>Combo {{ combo }}</v-btn>
+          <v-btn small>Sencilla {{ sencilla }}</v-btn>
+        </v-col>
+        <v-col cols="5">
+          <v-row no-gutters align="center">
+            <!--aqui deben mandar la info a el carrito de compras -->
+            <v-col>
+              <v-btn v-on:click="addDish" left="true" small>+</v-btn>
+            </v-col>
+            <v-col>
+              <v-text-field v-model="numDishes" id readonly solo />
+            </v-col>
+            <v-col>
+              <v-btn v-on:click="removeDish" small :disabled="numDishes < 1"
+                >-</v-btn
+              >
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="1">
+          <router-link :to="{ name: 'EditarPlatillo' }">
+            <v-btn icon>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </template>
@@ -59,16 +55,23 @@ export default {
     sencilla: {
       type: Number
     }
-    },
-    methods: {
+  },
+  methods: {
     addDish() {
       this.numDishes += 1;
     },
     removeDish() {
       this.numDishes -= 1;
     }
-    }
   }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.v-card__title {
+  padding: 2.5%;
+}
+.v-card__text {
+  padding: 1% 2.5%;
+}
+</style>

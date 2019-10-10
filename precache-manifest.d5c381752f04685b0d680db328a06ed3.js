4abd6f73d079488dbb1a606d5b19cfaf079a0785
Isis Lázaro <isislazaro@gmail.com>
2019-10-10 11:44:24 -0500

    deploy

---
+++ precache-manifest.d5c381752f04685b0d680db328a06ed3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dee6d75846eadafd779",
+    "url": "css/app.683148d5.css"
+  },
+  {
+    "revision": "5dee6d75846eadafd779",
+    "url": "js/app.7941ed99.js"
+  },
+  {
+    "revision": "4b1752af7052eff3b7be",
+    "url": "css/chunk-vendors.16aef84e.css"
+  },
+  {
+    "revision": "4b1752af7052eff3b7be",
+    "url": "js/chunk-vendors.81e6f2c4.js"
+  },
+  {
+    "revision": "db25515e6f9cb3a1c8d7fb81daeb12a5",
+    "url": "img/logo-comdi.db25515e.png"
+  },
+  {
+    "revision": "02380738c7f18f1d7e2cfd09a6b3e799",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
